f2f69f1d5a07812ad6a3bf6219693185ebd35fa9
Johnny Santiago <johnnyin2a@gmail.com>
2021-05-04 22:29:55 -0400

    Update root page title.

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Johnny Santiago'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Johnny\'s Next.js Sample Website'
 
 export default function Layout({children, home}) {
   return (
